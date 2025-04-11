f952e196e65856c04beeec397e793ae92ff8c3db
scotty974 <fabienetheve@hotmail.com>
2025-04-11 15:03:09 +0200

    add personnages

--- tp_django/dashboard/urls.py
+++ tp_django/dashboard/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, GamePersonnagesView
 urlpatterns = [
     path('dashboard/', IndexView.as_view(), name='dashboard'),
+    path('games/<int:game_id>/personnages/', GamePersonnagesView.as_view(), name='game-personnages'),
 ]
